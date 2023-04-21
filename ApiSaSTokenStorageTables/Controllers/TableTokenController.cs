def7d83a084f5d541220857e34dddc4380d94f9c
Carlos00-pixel <carlos.mayoral00@gmail.com>
2023-04-21 11:52:31 +0200

    Add project files.

---
+++ ApiSaSTokenStorageTables/Controllers/TableTokenController.cs
@@ -0,0 +1,39 @@
+﻿using ApiSaSTokenStorageTables.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSaSTokenStorageTables.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableTokenController : ControllerBase
+    {
+        private ServiceSaSToken service;
+
+        public TableTokenController(ServiceSaSToken service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        [Route("[action]/{curso}")]
+        public ActionResult<string> GenerateToken(string curso)
+        {
+            string token =
+                this.service.GenerateSasToken(curso);
+
+            //return Ok(
+            //    new
+            //    {
+            //        numeroregistros = 5,
+            //        datos = new List<int>()
+            //    });
+
+            return Ok(
+                new
+                {
+                    token = token
+                });
+        }
+    }
+}
